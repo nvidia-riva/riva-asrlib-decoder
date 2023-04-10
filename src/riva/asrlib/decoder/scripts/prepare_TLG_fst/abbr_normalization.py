caae6df18abf726162ea47182575be8fc5a6e490
Anand Joseph <anajoseph@nvidia.com>
2023-04-10 08:05:44 +0530

    Add scripts to get words from arpa lm and to deal with normalization
    Signed-off-by: Anand Joseph <anajoseph@nvidia.com>

---
+++ src/riva/asrlib/decoder/scripts/prepare_TLG_fst/abbr_normalization.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def abbr_map(abbr_mapfile, grammar_file):
+    abbr_maps = {}
+    with open(abbr_mapfile, 'r') as abbr_map_fp:
+        for line in abbr_map_fp:
+            (abbr, expansion) = line.strip().split('\t', maxsplit=1)
+            abbr_map[abbr] = expansion
+    if len(abbr_maps) > 0:
+        with open(grammar_file, 'r') as grm_ifp, open(grammar_file + '.exp', 'w') as ofp:
+            text = '\n'.join(grm_ifp.readlines())  # grammars are not expected to be large
+            for abbr in abbr_maps:
+                if abbr in text:
+                    text.replace(abbr, abbr_maps[abbr])
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: abbr_normalization.py <path_to_abbr_mapping file> <path to the grammar file>")
+
+    abbr_mapfile = sys.argv[1]  # this should be a tsv of format <abbreviation>\t<spoken_expansion>
+
+    grammar_file = sys.argv[2]  # this is the file containing list of entities with abbreviations to be expanded
+    abbr_map(abbr_mapfile, grammar_file)
